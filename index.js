3a0cf1b3437f95a94193cc4e96f1c2cc912149bb
anshuman <anshuman852@gmail.com>
2021-01-10 21:56:03 +0530

    add the bput shiz too

--- index.js
+++ index.js
@@ -134,6 +134,80 @@ app.get("/gift/tt/list", cache("1 day"), async (req, res) => {
       res.send();
     });
 });
+app.get("/bput/news", async (req, res) => {
+  try {
+    axios.get("http://bput.ac.in/news.php").then(async resp => {
+      $ = cheerio.load(resp.data);
+      let announcetable = $(
+        "#main_content > div.bodycontent > div > div:nth-child(2) > table > tbody"
+      );
+      cheerioTableparser($);
+      data = $("table").parsetable(false, false, false);
+      let message = [];
+      for (i = 0; i < 10; i++) {
+        num = data[0][i];
+        date = cheerio
+          .load(data[1][i])("span")
+          .text();
+        date=(date, dateFormat())
+        titledata = data[2][i];
+        title = cheerio
+          .load(titledata)("a")
+          .text();
+        link = cheerio
+          .load(titledata)("a")
+          .attr("href");
+        message.push({
+          num: num,
+          date: date,
+          title: title,
+          link: link
+        });
+      }
+      res.header("Content-Type", "application/json");
+      res.send(JSON.stringify(message, null, 4));
+    });
+  } catch {
+    res.status(404);
+    res.send("No idea");
+  }
+});
+app.get("/bput/exam", async (req, res) => {
+  try {
+    axios.get("http://bput.ac.in/exam-info.php").then(async resp => {
+      $ = cheerio.load(resp.data);
+      let announcetable = $(
+        "#main_content > div.bodycontent > div > div:nth-child(2) > table > tbody"
+      );
+      cheerioTableparser($);
+      data = $("table").parsetable(false, false, false);
+      let message = [];
+      for (i = 0; i < 10; i++) {
+        num = i + 1;
+        date = cheerio
+          .load(data[0][i])("span")
+          .text();
+        titledata = data[2][i];
+        title = cheerio
+          .load(titledata)("a")
+          .text();
+        link = cheerio
+          .load(titledata)("a")
+          .attr("href");
+        message.push({
+          num: num,
+          date: date,
+          title: title,
+          link: link
+        });
+      }
+      res.header("Content-Type", "application/json");
+      res.send(JSON.stringify(message, null, 4));
+    });
+  } catch {
+    res.send("No idea");
+  }
+});
 port= process.env.PORT || 3001
 app.listen(port, () => console.log(`listening on port 3001!`));
 
